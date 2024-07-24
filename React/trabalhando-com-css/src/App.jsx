4c886d29f740748a03edfb2a731d0f68bd8d6ec3
Vanderley <vanderleyoliv21@gmail.com>
2024-07-24 12:29:22 -0300

    up

---
+++ React/trabalhando-com-css/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const App = () => {
+  return (
+    <header>
+      <h1>Meu Site</h1>
+    </header>
+  );
+};
+
+export default App;
---
+++ React/trabalhando-com-css/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header` 
+background-color: #004aad; 
+color: white; 
+padding: 20px; 
+text-align: center; 
+`;
+
+const Nav = styled.nav` 
+display: flex; 
+justify-content: center; 
+gap: 20px; 
+`;
+
+const StyledLink = styled(NavLink)` 
+color: rgba(255, 255, 255, 0.8); 
+text-decoration: none; &.active, &:hover { 
+color: white; text-decoration: underline; } 
+`;
+
+const Header = () => {
+    return (
+        <HeaderContainer>
+            <h1>Meu Site</h1>
+            <Nav>
+                <StyledLink to="/" exact activeClassName="active">Home</StyledLink>
+                <StyledLink to="/products" activeClassName="active">Produtos</StyledLink>
+            </Nav>
+        </HeaderContainer>
+    );
+};
+
+export default Header;
